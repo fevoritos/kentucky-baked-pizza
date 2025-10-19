0aca48e941d1b652d78b5fc0368b18eb87ba99fa
fevoritos <fevoritos@mail.ru>
2025-10-19 12:59:43 +0300

    add prettier and tailwind

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,5 +20,11 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      'prettier': prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    }
   },
 ])
